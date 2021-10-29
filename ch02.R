ab99f7a9c229a59f0a0676c014756409b844b559
gmbneves <gmbneves@gmail.com>
2021-10-28 22:00:42 -0300

    Grammar check.

--- ch02.R
+++ ch02.R
@@ -19,7 +19,7 @@ ggplot(data = mpg)
 
 # Answer: An empty graph with no coordinates and plots appears.
 
-# 2. How many rows are in mtcars? How many colums?
+# 2. How many rows are in mtcars? How many columns?
 glimpse(mtcars)
 
 # Answer: mtcars has 32 rows and 11 columns.
@@ -29,17 +29,17 @@ glimpse(mtcars)
 # Answer: According to the help for the mpg, drv describes the type of drive
 # train, where f = front-wheel drive, r = rear wheel drive, and 4 = 4wd.
 
-# 4. Make a scatterplot of hwy versus cyl.
+# 4. Make a scatter plot of hwy versus cyl.
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = hwy, y = cyl))
 
-# 5. What happens if you make a scatterplot of class versus drv? Why is the plot
+# 5. What happens if you make a scatter plot of class versus drv? Why is the plot
 # not useful?
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = class, y = drv))
 
 # Answer: It plots a graphic showing what kind of drive train each car class
 # can have. The plot is not useful because class and drv are both qualitative
-# variables, so the scatterplot graphic doesn't show much.
+# variables, so the scatter plot graphic doesn't show much.
 
 ## Aesthetic Mappings  ----
